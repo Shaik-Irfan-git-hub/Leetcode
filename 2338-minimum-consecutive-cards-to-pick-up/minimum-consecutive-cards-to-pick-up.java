41dd173fb3a417e08f56c2b169f13af068afecf8
Shaik Irfan sharieff <irfan.skss786@gmail.com>
2026-08-11 16:04:34 +0530

    Time: 43 ms (68.32%) | Memory: 102.1 MB (69.45%) - LeetSync

--- 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.java
+++ 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.java
@@ -4,17 +4,14 @@ class Solution {
         int left=0;
         int min=Integer.MAX_VALUE;
         HashSet<Integer> set=new HashSet<>();
-        boolean dup=true;
         for(int i=0;i<n;i++){
             while(set.contains(cards[i])){
                 min=Math.min(min,i-left+1);
                 set.remove(cards[left]);
                 left++;
-                dup=false;
             }
             set.add(cards[i]);
         }
-        if(dup) return -1;
-        return min;
+        return min==Integer.MAX_VALUE ? -1 : min ;
     }
 }
