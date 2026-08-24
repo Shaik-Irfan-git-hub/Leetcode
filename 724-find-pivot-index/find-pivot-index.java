0a885fa0b3c17e23f2d44d01eb0ef5799dbc340f
Shaik Irfan sharieff <irfan.skss786@gmail.com>
2026-08-24 14:22:58 +0530

    Time: 1 ms (97.53%) | Memory: 46.8 MB (98.40%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n=nums.length;
+        int[] prefix=new int[n+1];
+        prefix[0]=0;
+        int ls=0;
+        int rs=0;
+        for(int i=0;i<n;i++){
+            rs+=nums[i];
+            prefix[i+1]+=rs;
+        }
+        for(int i=0;i<n;i++){
+            ls=prefix[i];
+            rs=prefix[n]-prefix[i+1];
+            if(ls==rs) return i;
+
+        }
+        return -1;
+    }
+}
