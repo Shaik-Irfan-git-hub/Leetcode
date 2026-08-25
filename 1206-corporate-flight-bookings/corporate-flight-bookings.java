eb73d873e442c5bd166e7dd81d3bc39ee35d6fe2
Shaik Irfan sharieff <irfan.skss786@gmail.com>
2026-08-25 14:58:22 +0530

    Time: 3 ms (98.38%) | Memory: 84.6 MB (63.41%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.java
@@ -1,16 +1,18 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
         int m=bookings.length;
-        int[] ans=new int[n];
-        
+        int[] ans=new int[n+1];
         for(int i=0;i<m;i++){
-            int left=bookings[i][0]-1;
-            int right=bookings[i][1]-1;
-            while(left<=right){
-                ans[left]+=bookings[i][2];
-                left++;
-            }
+            int a=bookings[i][0];
+            int b=bookings[i][1];
+            int c=bookings[i][2];
+            ans[a-1]+=c;          //difference array
+            ans[b]-=c;            //Range updates
         }
-        return ans;
+        for(int i=1;i<n;i++){
+            ans[i]+=ans[i-1];
+        }
+        return Arrays.copyOfRange(ans,0,n);
+        
     }
 }
